af4172abcd847dea805bd398fad90f2807bb1122
urcop <kirillzama@list.ru>
2021-09-27 09:35:24 +0300

    Почистил шаблон, обновил функции при старте диалога с ботом

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,6 +4,14 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 
 
+@dp.message_handler(CommandStart(deep_link='383212537'))
+async def teacher_link(message: types.Message):
+    text = (f'Здравствуйте, {message.from_user.full_name}!',
+            '<b>Пожалуйста пройдите небольшую регистрацию</b>',
+            '<b>Это займет буквально одну минуту!</b>')
+    await message.answer("\n".join(text))
+
+
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!")
